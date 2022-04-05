d08f626637765865e7a8f954e38217784223a805
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-04-05 12:53:34 +0800

    footer component boilerplate and header path set in app js file

--- 2.LandingPages/lawsuits/src/App.js
+++ 2.LandingPages/lawsuits/src/App.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import Button from "./sharedUI/Button";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <Fragment>
       <Button />
       <Header />
+      <Footer />
     </Fragment>
   );
 }
