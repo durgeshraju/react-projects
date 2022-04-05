c9729ee8ee6d4c943ed1d5647dadef19feafdd78
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-04-05 12:30:49 +0800

    Header component boilerplate

--- 2.LandingPages/lawsuits/src/App.js
+++ 2.LandingPages/lawsuits/src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Button from "./sharedUI/Button";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <Fragment>
       <Button />
+      <Header />
     </Fragment>
   );
 }
