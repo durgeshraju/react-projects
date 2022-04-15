baf9a72c0870f734c04d5ae5fa0a4c82cbaee6a1
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-04-15 09:26:11 +0800

    Header component essential calls

--- 2.LandingPages/lawsuits/src/components/Header/Header.js
+++ 2.LandingPages/lawsuits/src/components/Header/Header.js
@@ -1,8 +1,24 @@
-const Header = props =>{
-    return(
-        <header>
-            <h1>Header</h1>
+import classes from './Header.module.css'
+const Header = (props) => {
+    debugger
+    return(                 
+        <div className={["shadow-sm" + ' ' +"fixed-top"]}>               
+        <header className={classes.header}>               
+        <div className="container d-flex align-items-center justify-content-between">
+            {/*<h1 className={classes.logo}><a href={()=> false}></a></h1>*/}
+            <div className={classes.logo}><img alt="BootstrapMade" src={props.data ? props.data.logo: 'loading'} /></div>
+        </div>   
+        <div className="header-free-consulation-wrapper">
+        {props.data
+            ? props.data.HeaderRightContentCol.map((d, i) => (
+                <div>
+                <h2>{d.title}</h2>                                
+                </div>       
+        )): "loading"}        
+        </div>     
         </header>
+        </div>
+
     )
 }
 
