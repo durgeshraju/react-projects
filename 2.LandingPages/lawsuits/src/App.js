fdb68641d30313b4f478627da30c533cac6a4533
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-04-15 09:26:52 +0800

    ceplHeader attr added to call the json

--- 2.LandingPages/lawsuits/src/App.js
+++ 2.LandingPages/lawsuits/src/App.js
@@ -1,14 +1,22 @@
+import React, {useEffect, useState} from "react";
 import { Fragment } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HeroUnitBlock from "./components/HeroUnitBlock/HeroUnitBlock";
+import JsonData from "./content/data.json";
 
 function App() {
+const [landingPageData, setLandingPageData] = useState({});
+useEffect(() => {
+setLandingPageData(JsonData);
+}, []);
+console.log(landingPageData.ceplHeader);
   return (
     <Fragment>      
-      <Header />
-      <HeroUnitBlock />
-      <Footer />
+      <Header data={landingPageData.ceplHeader} />
+      {/*<Header data={landingPageData.ceplFeatures} />*/}
+      <HeroUnitBlock  />
+      {/*<Footer data={landingPageData.ceplTermsConditionsBlock} />*/}
     </Fragment>
   );
 }
