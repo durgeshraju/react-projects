65c4035cbe734fda7a514eb19e6c7494c37e725e
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-04-19 22:31:27 +0800

    Init condtional loader added

--- 2.LandingPages/lawsuits/src/App.js
+++ 2.LandingPages/lawsuits/src/App.js
@@ -6,14 +6,24 @@ import JsonData from "./content/data.json";
 
 function App() {
 const [landingPageData, setLandingPageData] = useState({});
+const [spinner, setSpinner ] = useState(true);
 useEffect(() => {
 setLandingPageData(JsonData);
+ setTimeout(() => setSpinner(false), 2500)
 }, []);
-//console.log(landingPageData.ceplHeader);
+
+console.log(landingPageData.ceplHeader);
   return (
-    <Fragment>      
-      <Header data={landingPageData.ceplHeader} />      
-      <HeroUnitBlock />      
+    <Fragment>
+      {spinner === true ? 
+      <Fragment>
+        <div className="loader"></div>  
+      </Fragment>  :
+        <Fragment>
+        <Header data={landingPageData.ceplHeader} />      
+        <HeroUnitBlock />        
+      </Fragment>
+      }
     </Fragment>
   );
 }
