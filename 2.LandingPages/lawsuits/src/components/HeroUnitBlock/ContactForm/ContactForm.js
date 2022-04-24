e5dd55a3042962b93c9b72c257c2892d368d4c48
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-04-24 19:33:51 +0800

    contactform

--- 2.LandingPages/lawsuits/src/components/HeroUnitBlock/ContactForm/ContactForm.js
+++ 2.LandingPages/lawsuits/src/components/HeroUnitBlock/ContactForm/ContactForm.js
@@ -1,8 +1,19 @@
+import React from 'react';
+import classes from './ContactForm.module.css'
+
 const ContactForm = props => {
     return(
-        <section>
-            <p>Contat form</p>
-        </section>
+        <div className= {classes['main-heading-wrapper']}>
+           <h3 className="fw-bold mb-0 pb-3"><span>Email now</span> for free legal advice</h3>
+            <div class="row">
+            <div class="col-lg-12">
+            <div class="form-group">
+            <span class="form-control-wrap your-name">
+            <input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" id="name" aria-required="true" aria-invalid="false" placeholder="Name" /></span>
+            </div>
+            </div>
+            </div>
+        </div>
     )
 }
 export default ContactForm;
