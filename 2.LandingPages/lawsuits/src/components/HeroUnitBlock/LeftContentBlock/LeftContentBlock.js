2a4da72216513b88eae2c31290d279dfe5c8b6b1
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-04-24 12:39:02 +0800

    Herounit leftcontent

--- 2.LandingPages/lawsuits/src/components/HeroUnitBlock/LeftContentBlock/LeftContentBlock.js
+++ 2.LandingPages/lawsuits/src/components/HeroUnitBlock/LeftContentBlock/LeftContentBlock.js
@@ -8,7 +8,7 @@ const LeftContentBlock = props => {
                 <Fragment key={i}>
                 <div className={classes.herounitPrimaryContentSection}>
                 <h1 className="display-6 fw-bolder mb-2">{d.title}</h1>        
-                <p className="lead fw-bold text-white mb-5">{d.description}</p>      
+                <p className="lead text-white mb-5">{d.description}</p>      
                 </div>            
                 </Fragment>                    
                 )) : "loading"                
@@ -16,8 +16,11 @@ const LeftContentBlock = props => {
             {props.data
               ? props.data.secondaryContentWidget.map((d, i) => (
                 <Fragment key={i}>
+                <div className={classes.herounitSecondaryContentSection}>
                 <h2 className="fw-bolder mb-2">{d.title}</h2>        
-                <p className="lead fw-bold text-white mb-5">{d.description}</p>                  
+                <p className="lead text-white mb-3">{d.description}</p>   
+                <h3><span className="fs-4">Call Us: </span><a href="tel:+6494461709">{d.phone}</a></h3>    
+                </div>           
                 </Fragment>                    
                 )) : "loading"                
             }    
