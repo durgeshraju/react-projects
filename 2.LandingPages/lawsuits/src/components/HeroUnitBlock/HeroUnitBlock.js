f7f846615e65cb896ad4d6ca11791c20904a04d9
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-04-24 19:32:47 +0800

    hero unit section enhancement

--- 2.LandingPages/lawsuits/src/components/HeroUnitBlock/HeroUnitBlock.js
+++ 2.LandingPages/lawsuits/src/components/HeroUnitBlock/HeroUnitBlock.js
@@ -3,6 +3,7 @@ import LeftContentBlock from "./LeftContentBlock/LeftContentBlock";
 import ContactForm from "./ContactForm/ContactForm";
 import HeroUnitData from "../../content/HeroUnitData.json";
 import classes from './HeroUnitBlock.module.css';
+import Card from "../../sharedUI/Card";
 
 const HeroUnitBlock = props => {
 const [landingPageData, setLandingPageData] = useState({});
@@ -12,14 +13,16 @@ setLandingPageData(HeroUnitData);
     return(        
     <section className={classes['herounit-wrapper']}>   
         <div className="container px-5 py-5">
-            <div className="row gx-5 align-items-center justify-content-center">
-                <div className="col-lg-8 col-xl-7 col-xxl-7">
-                    <div className="my-5 text-center text-xl-start">                    
+            <div className="row gx-5 justify-content-center">
+                <div className="col-lg-8 col-xl-8 col-xxl-8">
+                    <div className="my-3 text-center text-xl-start">                    
                         <LeftContentBlock data={landingPageData.ceplHeroUnitContent}  />
                     </div>
                 </div>
-                <div className="col-xl-5 col-xxl-5 d-none d-xl-block text-center">
-                    <ContactForm />
+                <div className="col-xl-4 col-xxl-4 d-none d-xl-block my-3">
+                    <Card>
+                        <ContactForm />
+                    </Card>
                 </div>
             </div>
         </div>         
