6a223bc115694badb58f063464992f55aecf7895
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-04-26 08:55:10 +0800

    leftcontentblock tweaks

--- 2.LandingPages/lawsuits/src/components/HeroUnitBlock/LeftContentBlock/LeftContentBlock.js
+++ 2.LandingPages/lawsuits/src/components/HeroUnitBlock/LeftContentBlock/LeftContentBlock.js
@@ -7,8 +7,10 @@ const LeftContentBlock = props => {
               ? props.data.primaryContentWidget.map((d, i) => (
                 <Fragment key={i}>
                 <div className={classes.herounitPrimaryContentSection}>
+                <div className="my-5">
                 <h1 className="fw-bolder mb-3">{d.title}</h1>        
                 <p className="lead text-white mb-5 fs-5">{d.description}</p>      
+                </div>
                 </div>            
                 </Fragment>                    
                 )) : "loading"                
@@ -16,10 +18,12 @@ const LeftContentBlock = props => {
             {props.data
               ? props.data.secondaryContentWidget.map((d, i) => (
                 <Fragment key={i}>
-                <div className={classes.herounitSecondaryContentSection}>
+                <div className="my-5">
+                <div className={classes.herounitSecondaryContentSection}>                
                 <h2 className="fw-bolder mb-2">{d.title}</h2>        
                 <p className="lead text-white mb-3">{d.description}</p>   
-                <h3><span className="fs-4">Call Us: </span><a href="tel:+6494461709">{d.phone}</a></h3>    
+                <h3><span className="fs-4">Call Us: </span><a href="tel:+6494461709">{d.phone}</a></h3>  
+                </div>
                 </div>           
                 </Fragment>                    
                 )) : "loading"                
