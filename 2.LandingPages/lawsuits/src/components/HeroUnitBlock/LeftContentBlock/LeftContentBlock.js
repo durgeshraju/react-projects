dcf5447c6a52e11b01f0ae4d623026f0bd37f6f4
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-04-30 13:00:17 +0800

    leftcontent block classes enchancement

--- 2.LandingPages/lawsuits/src/components/HeroUnitBlock/LeftContentBlock/LeftContentBlock.js
+++ 2.LandingPages/lawsuits/src/components/HeroUnitBlock/LeftContentBlock/LeftContentBlock.js
@@ -1,30 +1,26 @@
 import React, {Fragment} from "react";
 import classes from './LeftContentBlock.module.css';
-const LeftContentBlock = props => {
+const LeftContentBlock = props => {  
     return(
         <Fragment>                        
             {props.data
               ? props.data.primaryContentWidget.map((d, i) => (
-                <Fragment key={i}>
-                <div className={classes.herounitPrimaryContentSection}>
-                <div className="my-5">
+                <Fragment key={i}>                
+                <div className={`${classes['herounitPrimaryContentSection']} my-5`}>                
                 <h1 className="fw-bolder mb-3">{d.title}</h1>        
-                <p className="lead text-white mb-5 fs-5">{d.description}</p>      
-                </div>
+                <p className="lead text-white mb-5 fs-5">{d.description}</p>                      
                 </div>            
                 </Fragment>                    
                 )) : "loading"                
             }  
             {props.data
               ? props.data.secondaryContentWidget.map((d, i) => (
-                <Fragment key={i}>
-                <div className="my-5">
-                <div className={classes.herounitSecondaryContentSection}>                
+                <Fragment key={i}>                
+                <div className={`${classes['herounitSecondaryContentSection']} my-5`}>                
                 <h2 className="fw-bolder mb-2">{d.title}</h2>        
                 <p className="lead text-white mb-3">{d.description}</p>   
                 <h3><span className="fs-4">Call Us: </span><a href="tel:+6494461709">{d.phone}</a></h3>  
-                </div>
-                </div>           
+                </div>                                       
                 </Fragment>                    
                 )) : "loading"                
             }    
