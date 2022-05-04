4b463478a280fe26da36ee62df8c13e51975eb46
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-05-04 18:39:19 +0800

    contact form with vadation data

--- 2.LandingPages/lawsuits/src/components/HeroUnitBlock/ContactForm/ContactForm.js
+++ 2.LandingPages/lawsuits/src/components/HeroUnitBlock/ContactForm/ContactForm.js
@@ -1,19 +1,75 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import classes from './ContactForm.module.css'
+import InputField from '../../../sharedUI/InputField';
 
-const ContactForm = props => {
-    return(
-        <div className= {classes['main-heading-wrapper']}>
-           <h3 className="fw-bold mb-0 pb-3"><span>Email now</span> for free legal advice</h3>
-            <div class="row">
-            <div class="col-lg-12">
-            <div class="form-group">
-            <span class="form-control-wrap your-name">
-            <input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-control" id="name" aria-required="true" aria-invalid="false" placeholder="Name" /></span>
-            </div>
-            </div>
-            </div>
+const ContactForm = props => {    
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    phone: "",    
+    helpFiled: ""
+  });  
+  const userInput = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      label: "Username",
+      placeholder: "Username",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      label: "Email Address",
+      placeholder: "Email Address",
+      required: true
+    },
+    {
+      id: 3,
+      name: "phone",
+      type: "tel",
+      label: "Phone",
+      placeholder: "Phone Number",     
+      required: true
+    },
+    {
+      id: 4,
+      name: "helpFiled",
+      type: "text",
+      label: "Please Key in your query",
+      placeholder: "How can we help ?",     
+      required: false      
+    }
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+   const onChangeHandler = (e) =>{    
+    setValues({ ...values, [e.target.name]: e.target.value});    
+   }
+
+    return(   
+        <form onSubmit={handleSubmit}>
+        <div className= {classes['main-heading-wrapper']}>          
+           <h3 className="fw-bold mb-0 pb-3"><span>Email now</span> for free legal advice</h3>   
+           <div className= {classes.formElemntsWrapper}>
+            {userInput.map((input)=>{
+            return (
+            <Fragment key={input.id}>
+            <InputField                         
+              {...input} 
+               value={values[input.name]}
+              onChange={onChangeHandler}
+            /></Fragment>
+            )})}
+            </div>            
+            <button type="submit" className="btn btn-lg btn-primary w-100 mt-1">Start Free Consultation</button>
         </div>
+        </form>            
     )
 }
 export default ContactForm;
