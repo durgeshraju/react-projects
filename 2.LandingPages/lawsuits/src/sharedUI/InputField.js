8d37cf79f5d92b726ea6c8b32e98e08c900bb6a7
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-05-04 17:54:21 +0800

    inputFiled component props added

--- 2.LandingPages/lawsuits/src/sharedUI/InputField.js
+++ 2.LandingPages/lawsuits/src/sharedUI/InputField.js
@@ -1,15 +1,17 @@
 import React from 'react';
-const InputField = ({ value, label, name, placeholder, type, onChange }) => {    
+const InputField = ({ value, label, name, placeholder, type, onChange, required}) => { 
+        
     return(                
         <div className="mb-2">                       
         {label && <label htmlFor="input-field">{label}</label>}     
         <input
             type={type}
             value={value}
-            name={name}
-            className="form-control"
-            placeholder={placeholder}
+            name={name}            
+            placeholder={placeholder}            
             onChange={onChange}
+            required ={required}
+            className="form-control"
         />
         </div>
     )
