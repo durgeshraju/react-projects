68dcbeadbfa1d68304811f1fbff658ace73e67ad
ndurgesh88 <durgeshnidadavolu@gmail.com>
2022-05-04 19:17:14 +0800

    added React mediaQuery condition to header phone

--- 2.LandingPages/lawsuits/src/components/Header/Header.js
+++ 2.LandingPages/lawsuits/src/components/Header/Header.js
@@ -1,24 +1,26 @@
-import React, {Fragment, useState} from 'react'
+import React, {Fragment} from 'react'
 import classes from './Header.module.css';
+import MediaQuery from 'react-responsive'
 const Header = (props) => {             
     return(                                        
       <header className="py-3 border-bottom shadow-sm bg-white">               
-        <div className="container d-flex align-items-center justify-content-between">           
+        <div className="container d-flex align-items-center justify-content-between">            
             <div className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
                 <img alt="BootstrapMade" src={props.data ? props.data.logo: 'loading'} />
-            </div>                                          
-                
-        <div className="col-12 col-lg-auto mb-3 mb-lg-0">
-            <div className={classes['h-phone-wrapper']}>
-              {props.data
-                ? props.data.HeaderRightContentCol.map((d, i) => (
-                <Fragment key={i}>                                
-                <span>{d.title}</span><i className="fa fa-phone"></i> {d.phone}                
-                </Fragment>       
-            ))
-            : "loading"}                           
-            </div>    
-        </div>   
+            </div>                                                       
+        <MediaQuery minWidth={992}>                    
+          <div className="col-12 col-lg-auto mb-3 mb-lg-0">
+              <div className={classes['h-phone-wrapper']}>
+                {props.data
+                  ? props.data.HeaderRightContentCol.map((d, i) => (
+                  <Fragment key={i}>                                
+                  <span>{d.title}</span><i className="fa fa-phone"></i> {d.phone}                
+                  </Fragment>       
+              ))
+              : "loading"}                           
+              </div>    
+          </div>   
+        </MediaQuery>
         </div>    
       </header>        
     )
