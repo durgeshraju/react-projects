a38f1db5ba130e9c33c2e55568560cb2629ddf9b
ndurgesh88 <durgeshnidadavolu@gmail.com>
2023-04-09 13:52:50 +0800

    Remove 7.signup-form project

--- 7.signup-form/src/App.js
+++ 7.signup-form/src/App.js
@@ -2,9 +2,12 @@ import './styles/main.css';
 
 function App() {
   return (
-    <main>
-        <h1>Testing</h1>
-    </main>
+    <div className="intro-herounit">
+      <div className="intro-herounit__content">
+        <h1 className="intro-herounit__content__heading">Learn to code by watching others</h1>
+        <p className="intro-herounit__content__paragraph">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+      </div>
+    </div>
   );
 }
 
